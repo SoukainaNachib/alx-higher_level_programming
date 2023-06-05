7e30d698381e1f08e06da639fdaac4d07a720af4
soukainanachib <soukaina.nachib23@gmail.com>
2023-06-05 15:06:48 -0700

    header file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,7 +5,7 @@
 
 /**
  * struct listint_s - Singly Linked List
- * @x: Integer
+ * @n: Integer
  * @next: Points To The Next Node
  *
  * Description: Singly Linked List Node Struct
@@ -13,12 +13,12 @@
  */
 typedef struct listint_s
 {
-	int x;
+	int n;
 	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int x);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
