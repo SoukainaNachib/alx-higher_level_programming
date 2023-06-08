b38a00982e3d8ec1c841ff5eca6d3ae19336b2aa
soukainanachib <soukaina.nachib23@gmail.com>
2023-06-08 05:30:49 -0700

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
+
 from calculator_1 import add, sub, mul, div
 
-if __name__ == '__main__':
-    """
-
-    The result of the addition, substract,division
-    and multiplication between two numbers a and b
-
-    """
+if __name__ == "__main__":
     a = 10
     b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
-    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
